d391a05377e70f5ca9a3487be080897e618fcf02
waschgeht <ainetter.martin@gmail.com>
2020-12-06 18:04:25 +0100

    Ersetllung des Files 06.12.2020

---
+++ functions.py
@@ -0,0 +1,13 @@
+import os
+
+
+'''funktion requestet ip von ifconfig.me'''
+def external_ip_requester():
+    try:
+        ip = str(os.popen("curl http://ifconfig.me/ip").read()) #.popen erzeugt keine Ausgabe am Screen
+        int(ip[0]) #testet ob ausgabe eine IP ist. Erzeugt fehler wenn falsch und läuft in except block
+        return ip
+    except:
+        print("Couldnt get ip from http://ifconfig.me/ip")
+        raise SystemExit(0) #schließt das programm komplett
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import ip_tracker.functions as f
+
+a = f.external_ip_requester()
+print(a)
