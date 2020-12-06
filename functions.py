09c5fadd424e9b0c28f7358d9032b108d9080f9d
waschgeht <ainetter.martin@gmail.com>
2020-12-06 21:34:46 +0100

    06.12.2020 hinzufügen der EMailfunktion und logging

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import os
-
+import smtplib
+from _datetime import datetime
+from email.message import EmailMessage
 
 '''funktion requestet ip von ifconfig.me'''
 def external_ip_requester():
@@ -11,3 +13,32 @@ def external_ip_requester():
         print("Couldnt get ip from http://ifconfig.me/ip")
         raise SystemExit(0) #schließt das programm komplett
 
+
+'''
+Sendet plain text message
+'''
+def send_text():
+    with open(data.conf, "r") as data:
+        Data = data.readline()
+    try:
+        message = 'Subject: {}\n\n{}'.format("Your current IP", "Your current IP is " + Data[2])
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.login(Data[0], Data[1])
+        server.sendmail(Data[0], Data[3], message)
+        server.quit()
+    except Exception as error:
+        print("Error, couldn't send message")
+        print(error)
+        logging("Couldn't send email")
+
+
+def logging(TEXT):
+    folder_loacation = os.getcwd()
+    file_location = folder_loacation + "\\email.log"
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        with open(file_location, "a+") as log:
+            log.write(date + ";   " + TEXT  + " \n")
+    except Exception as nopen1:
+        print("Couldn't write to file")
+        print(nopen1)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import ip_tracker.functions as f
 
-a = f.external_ip_requester()
-print(a)
+ip = f.external_ip_requester()
 
+email= input("please enter a username:")
+password = input(" enter a password")
+
+with open("data.conf", "w+") as file:
+    file.writelines([email, "\n", password, "\n", ip])
