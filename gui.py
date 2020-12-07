7ac479a8c205de1484718e3a322ee7cce94b8515
waschgeht <ainetter.martin@gmail.com>
2020-12-07 22:13:27 +0100

    07.12.2020 addet first two test inputs. commit weil weirde probleme mit import der funktionen

--- gui.py
+++ gui.py
@@ -11,3 +11,14 @@ except:
     root.destroy()
     f.logging("Startwindow failure")
 
+try:
+    root = tk.Tk()
+    tk.Label(root, text="First Name").grid(row=0)
+    tk.Label(root, text="Last Name").grid(row=1)
+    e1 = tk.Entry(root)
+    e2 = tk.Entry(root)
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+    root.mainloop()
+except:
+    root.destroy()
